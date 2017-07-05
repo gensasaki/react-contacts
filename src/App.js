7d4a374aa29e2ff4216f4e507778b1155cabb9b4
Gen Sasaki <sasakigen11@gmail.com>
2017-07-05 00:08:42 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContactList contacts={[
+          { name: 'Micheal' },
+          { name: 'Ryan' },
+          { name: 'Tyler' }
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Chai' },
+          { name: 'Json' }
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Sean' },
+          { name: 'Nisa' },
+          { name: 'Milky' }
+        ]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
