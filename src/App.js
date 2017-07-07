1e34206f53326aecf19d9eabc08e6ab5c11d8b7e
Gen Sasaki <sasakigen11@gmail.com>
2017-07-06 22:29:09 -0700

    Pass Data With Props

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Micheal' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Chai' },
-          { name: 'Json' }
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Sean' },
-          { name: 'Nisa' },
-          { name: 'Milky' }
-        ]}/>
+      <div>
+        <ListContacts contacts={contacts} />
       </div>
-    );
+    )
   }
 }
 
